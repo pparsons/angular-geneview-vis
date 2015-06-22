acd769561ded71a8ae75bc9b5db62dee43a05025
Joe Lin <linjoeyy@gmail.com>
2015-06-22 16:43:17 -0400

    adding gen2phen.php to make repo standalone complete; code by Paul Parsons

---
+++ src/server/php/gen2phen.php
@@ -0,0 +1,37 @@
+<?php
+/*
+php endpoint for cytognomix/jbrowse page
+March 2015
+author: paul parsons
+pparsons@uwo.ca
+*/
+
+//OMIM api key
+$ApiKey = '';
+
+
+//get the input from the client
+$gene = $_GET['gene'];
+$curl = curl_init();
+                                      
+//the url to send to
+$omim_url = 'http://api.omim.org/api/entry/search?search=' . $gene . '&sort=score&include=geneMap&format=json';
+
+curl_setopt_array($curl, array(
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => $omim_url,
+        CURLOPT_CONNECTTIMEOUT => 0,
+        CURLOPT_VERBOSE => true,
+				CURLOPT_HTTPHEADER => array('ApiKey:' . $ApiKey)
+    ));
+
+$result = curl_exec($curl);
+
+curl_close($curl);
+
+//send the result to the client
+echo $result;
+
+        
+        
+        
